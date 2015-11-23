15fee86f8213241cd2ddebb20ebbb3795680250a
Artem Govorov <artem.govorov@gmail.com>
2015-11-23 19:57:04 +1000

    Updated sail.js wallaby config

--- wallaby.js
+++ wallaby.js
@@ -9,73 +9,93 @@ module.exports = function () {
     tests: ['test/unit/**/*.test.js'],
 
     bootstrap: function (wallaby) {
-      if (global.sails) return;
-
-      var freeport = require('freeport');
-      var path = require('path');
-      var cp = require('child_process');
-      var fs = require('fs');
-      var fork = cp.fork;
-      var existsSync = fs.existsSync;
-
-      // Replacing fork to make sails start grunt in local project folder
-      cp.fork = function (file, args, opts) {
-        if (file.indexOf('grunt-cli')) {
-          opts.cwd = wallaby.localProjectDir;
-        }
-        return fork.apply(this, arguments);
-      };
-
-      // Replacing existsSync to make sails not throw error when it doesn't find node module in wallaby cache.
-      // Wallaby will use local node module anyway.
-      fs.existsSync = function (filePath) {
-        if (filePath && filePath.indexOf(path.join('instrumented', 'node_modules'))) {
-          return true;
-        }
-        return existsSync.apply(this, arguments);
-      };
 
       wallaby.delayStart();
 
-      wallaby.testFramework.ui('bdd');
+      var setup = function () {
 
-      var Sails = require('sails');
-      var should = require('should');
+        var freeport = require('freeport');
+        var path = require('path');
+        var cp = require('child_process');
+        var fs = require('fs');
+        var fork = cp.fork;
+        var existsSync = fs.existsSync;
 
-      var Barrels = require('barrels');
-
-      freeport(function (err, port) {
-        if (err) throw err;
-
-        // Lift Sails with test database
-        global.sails = Sails.lift({
-          log: {
-            level: 'info'
-          },
-          models: {
-            connection: 'test',
-            migrate: 'drop'
-          },
-          port: port
-        }, function (err) {
-          if (err) {
-            if (err) throw err;
-            wallaby.start();
+        // Replacing buildDictionary.optional to resolve hooks from local node_modules
+        var buildDictionary = require('sails/node_modules/sails-build-dictionary');
+        var originalOptional = buildDictionary.optional;
+        buildDictionary.optional = function () {
+          if (~arguments[0].dirname.indexOf('node_modules')) {
+            arguments[0].dirname = path.join(wallaby.localProjectDir, 'node_modules');
           }
+          return originalOptional.apply(this, arguments);
+        };
 
-          // Load fixtures
-          var barrels = new Barrels();
+        // Replacing fork to make sails start grunt in local project folder
+        cp.fork = function (file, args, opts) {
+          if (file.indexOf('grunt-cli')) {
+            opts.cwd = wallaby.localProjectDir;
+          }
+          return fork.apply(this, arguments);
+        };
 
-          // Save original objects in `fixtures` variable
-          fixtures = barrels.data;
+        // Replacing existsSync to make sails not throw error when it doesn't find node module in wallaby cache.
+        // Wallaby will use local node module anyway.
+        fs.existsSync = function (filePath) {
+          if (filePath && filePath.indexOf(path.join('instrumented', 'node_modules'))) {
+            return true;
+          }
+          return existsSync.apply(this, arguments);
+        };
 
-          // Populate the DB
-          barrels.populate(function (err) {
-            if (err) throw err;
-            wallaby.start();
+        var Sails = require('sails').Sails;
+        var should = require('should');
+
+        var Barrels = require('barrels');
+
+        freeport(function (err, port) {
+          if (err) throw err;
+
+          // Lift Sails with test database
+          global.sails = new Sails();
+          global.sails.load({
+            log: {
+              level: 'info'
+            },
+            models: {
+              connection: 'test',
+              migrate: 'drop'
+            },
+            port: port
+          }, function (err) {
+            if (err) {
+              if (err) throw err;
+              wallaby.start();
+            }
+
+            // Load fixtures
+            var barrels = new Barrels();
+
+            // Save original objects in `fixtures` variable
+            fixtures = barrels.data;
+
+            // Populate the DB
+            barrels.populate(function (err) {
+              if (err) throw err;
+              wallaby.start();
+            });
           });
         });
-      });
+      };
+
+      // if it doesn't work for you, use workers: {recycle: true}
+      // http://wallabyjs.com/docs/config/workers.html
+      if (global.sails) {
+        global.sails.lower(setup);
+      }
+      else {
+        setup();
+      }
     },
     env: {
       type: 'node'
